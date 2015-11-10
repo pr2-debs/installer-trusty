2cb13327c2293cb528366eb726ecf35368e77d25
TheDash <dash@clearpathrobotics.com>
2015-11-10 15:39:28 -0500

    Updated recursive deps scripts

--- basestation/recursive-deps.sh
+++ basestation/recursive-deps.sh
@@ -3,9 +3,9 @@
 # Update package lists
 rm deplist.pkgs
 ./fetch
-./repocheck release basestation-release.yaml --path install_dev1/pr2-packages --gen 0.7.0
+./repocheck release basestation-release.yaml --path install_dev1/pr2-packages --gen 0.9.0
 ./verifyinstaller
 
 # Copy the deplist.pkgs
-scp deplist.pkgs dash@dev-vm-packages-01:/var/repos/pr2packages/ubuntu/conf/
+scp deplist.pkgs dash@dev-vm-packages-01:/var/repos/pr2-dev-internal/ubuntu/conf/
 ssh -t -t dash@dev-vm-packages-01 'bash -s' < recursive-deps2.sh
--- basestation/recursive-deps2.sh
+++ basestation/recursive-deps2.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-cd /var/repos/pr2packages/ubuntu/conf
+cd /var/repos/pr2-dev-internal/ubuntu/conf
 cat deplist.pkgs >> trustydeps.packages
 awk '!seen[$0]++' trustydeps.packages > trustydepsnew.packages
 mv trustydepsnew.packages trustydeps.packages
+cp trustydeps.packages trustydepsi386.packages
+sed -e 's/^[^ ]*\>/&:i386/'
 cd ../
 reprepro -Vb . --noskipold update trusty
